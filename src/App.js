50adbd8fba181cc1c3d93c3394777796952e53ee
Pavithran Ambur Venkatesan <support@eastghats.com>
2025-04-29 02:15:04 +0530

    Initial Hello World React setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Hello World 🌍 - CCP UI</h1>
+    </div>
+  );
+}
